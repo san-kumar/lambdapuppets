072ee23d2d6c481ade28f2196b7434c3dd1b1cf8
Your Name <you@example.com>
2020-04-01 00:15:21 +0530

    updated example

--- puppets/example.js
+++ puppets/example.js
@@ -4,11 +4,3 @@ module.exports.run = async (browser, event) => {
 
     return await page.title();
 };
-
-module.exports.config = {
-    pipe: {
-        telegram: {bot_id: 'bot639867240:AAFB_skcLXfG2z1JzWXvnm4wN6N9W0NvT-Y', chat_id: '730349437'},
-        email: {from: 'san@uploader.win', to: 'sanchitbh@gmail.com', subject: 'yo check this'},
-        url: {target: 'https://xxx.requestcatcher.com/test'},
-    }
-};
